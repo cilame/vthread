57ded5e60a8f2f77a0924be6e0ee070d5890ad68
cilame <opaquism@hotmail.com>
2018-07-14 19:29:20 +0800

    修改 setup 说明文档。

--- setup.py
+++ setup.py
@@ -4,13 +4,33 @@ import sys
   
 setup(  
     name="vthread",
-    version="0.0.6",
+    version="0.0.8",
     author="vilame",
     author_email="opaquism@hotmail.com",
     description="the best threadpool pack.",
-    long_description="import vthread; help(vthread)",
+    long_description="""
+#### You can implement thread pools by adding a single line of code without changing the order of any previous code.
+- Example
+```python
+import vthread
+
+@vthread.pool(3) # Create three thread pools
+def crawl(i):
+    import time;time.sleep(1) # Simulation time consuming
+    print("crawl_url:",i)
+
+urls = ["http://url1",
+        "http://url2",
+        "http://url3",
+        "http://url4"]
+
+for u in urls:
+    crawl(u) # This function becomes a function that adds the original function to the thread pool.
+```
+""",
+    long_description_content_type="text/markdown",
     license="MIT",
-    url="https://pypi.org/project/vthread/",
+    url="https://github.com/cilame/vthread",
     packages=['vthread'],
     classifiers=[
         "Environment :: Web Environment",
