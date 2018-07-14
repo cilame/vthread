86071715843d3c36c6d541977cb1ea13fb9732d4
cilame <2473495041@qq.com>
2018-07-14 11:00:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import sys
   
 setup(  
     name="vthread",
-    version="0.0.4",
+    version="0.0.5",
     author="vilame",
     author_email="opaquism@hotmail.com",
-    description="simple threading pack.",
+    description="the best threadpool pack.",
     long_description="import vthread; help(vthread)",
     license="MIT",
     url="https://pypi.org/project/vthread/",
