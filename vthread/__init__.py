6625701ddd17dd26c635a9c41f364031742f7519
cilame <2473495041@qq.com>
2018-07-17 15:34:36 +0800

    Update __init__.py

--- vthread/__init__.py
+++ vthread/__init__.py
@@ -1,3 +1,8 @@
 from .vthread import *
 
 __all__ = vthread.__all__
+__title__ = 'vthread'
+__description__ = 'the best threadpool pack.'
+__version__ = '0.1.0'
+__author__ = 'vilame'
+__author_email__ = 'opaquism@hotmail.com'
