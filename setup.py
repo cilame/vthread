25d5297f98dfde7dbbc6caae844c7bc0fc94cc9c
cilame <2473495041@qq.com>
2018-08-05 09:36:34 +0800

    fix bug.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
   
 setup(  
     name="vthread",
-    version="0.1.0",
+    version="0.1.1",
     author="vilame",
     author_email="opaquism@hotmail.com",
     description="the best threadpool pack.",
