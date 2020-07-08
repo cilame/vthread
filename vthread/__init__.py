6ebbaccf1f997242cad5802e41f804d0757a414b
cilame <opaquism@hotmail.com>
2020-07-09 00:58:04 +0800

    add

--- vthread/__init__.py
+++ vthread/__init__.py
@@ -3,6 +3,6 @@ from .vthread import *
 __all__ = vthread.__all__
 __title__ = 'vthread'
 __description__ = 'the best threadpool pack.'
-__version__ = '0.1.0'
+__version__ = '0.1.4'
 __author__ = 'vilame'
 __author_email__ = 'opaquism@hotmail.com'
